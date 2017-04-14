93da4ee2e5f0062026e63fa67c5b269f5c02e1e0
florent champigny <florent.champigny@backelite.com>
2017-04-14 16:50:41 +0200

    Removed unused code

--- app/src/main/java/com/github/florent37/youtubemp3downloader/repository/FileDownloaderRepositoryImpl.java
+++ app/src/main/java/com/github/florent37/youtubemp3downloader/repository/FileDownloaderRepositoryImpl.java
@@ -8,15 +8,11 @@ import com.github.florent37.youtubemp3downloader.Video;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
-import io.reactivex.ObservableTransformer;
 import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -38,77 +34,53 @@ public class FileDownloaderRepositoryImpl implements FileDownloaderRepository {
         this.okHttpClient = okHttpClient;
     }
 
-    public <V> ObservableTransformer<V, V> retryTransformer() {
-        return new ObservableTransformer<V, V>() {
-
-            @Override
-            public ObservableSource<V> apply(Observable<V> upstream) {
-                return upstream.retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
-                    @Override
-                    public ObservableSource<?> apply(@NonNull Observable<Throwable> throwableObservable) throws Exception {
-                        return Observable.just(1); //ton code ici
-                    }
-                });
-            }
-        };
-    }
-
     @Override
     public Observable<Video> download(final Context context, Video video) {
-        Observable.just(Arrays.asList("1", "2"))
-                .compose(retryTransformer())
-                .subscribe(new Consumer<List<String>>() {
-                    @Override
-                    public void accept(@NonNull List<String> result) throws Exception {
+        return Observable
+                .create((ObservableOnSubscribe<File>) subscription -> {
+                    final Request request = new Request.Builder()
+                            .url(video.getLink())
+                            .build();
 
+                    okHttpClient.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            subscription.onError(e);
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            final String fileName = video.getTitle().replaceAll("[^a-zA-Z]+", "");
+                            final File file = new File(Environment.getExternalStorageDirectory(), "/Download/" + fileName + ".mp3");
+
+                            try {
+                                file.createNewFile();
+
+                                BufferedSink sink = Okio.buffer(Okio.sink(file));
+                                sink.writeAll(response.body().source());
+                                sink.close();
+                            } catch (Exception e) {
+                                subscription.onError(e);
+                                throw e;
+                            }
+
+                            subscription.onNext(file);
+                            subscription.onComplete();
+                        }
+                    });
+                })
+                .flatMap(new Function<File, ObservableSource<Video>>() {
+                    @Override
+                    public ObservableSource<Video> apply(@NonNull File file) throws Exception {
+                        return Observable.create(e -> MediaScannerConnection.scanFile(
+                                context,
+                                new String[]{file.getAbsolutePath()},
+                                null,
+                                (path, uri) -> {
+                                    e.onNext(video);
+                                    e.onComplete();
+                                }));
                     }
                 });
-
-
-        return Observable.create((ObservableOnSubscribe<File>) subscription -> {
-
-            final Request request = new Request.Builder()
-                    .url(video.getLink())
-                    .build();
-
-            okHttpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    subscription.onError(e);
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    final String fileName = video.getTitle().replaceAll("[^a-zA-Z]+", "");
-                    final File file = new File(Environment.getExternalStorageDirectory(), "/Download/" + fileName + ".mp3");
-
-                    try {
-                        file.createNewFile();
-
-                        BufferedSink sink = Okio.buffer(Okio.sink(file));
-                        sink.writeAll(response.body().source());
-                        sink.close();
-                    } catch (Exception e) {
-                        subscription.onError(e);
-                        throw e;
-                    }
-
-                    subscription.onNext(file);
-                    subscription.onComplete();
-                }
-            });
-        }).flatMap(new Function<File, ObservableSource<Video>>() {
-            @Override
-            public ObservableSource<Video> apply(@NonNull File file) throws Exception {
-                return Observable.create(e -> MediaScannerConnection.scanFile(
-                        context,
-                        new String[]{file.getAbsolutePath()},
-                        null,
-                        (path, uri) -> {
-                            e.onNext(video);
-                            e.onComplete();
-                        }));
-            }
-        });
     }
 }
